2a204ecdcb88be6f7e572fff459d04ab201fb476
Aisha Mirza <84517249+ayshenm@users.noreply.github.com>
2024-01-30 02:10:03 +0400

    Authorizations have expired

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,33 @@
-import styles from '@/styles/Home.module.css'
-// import { NextPageContext } from 'next';
-// import { getSession } from 'next-auth/react';
+import useCurrentUser from "@/hooks/useCurrentUser";
+import styles from "@/styles/Home.module.css";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
 
-// export async function getServerSideProps(context: NextPageContext) {
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
 
-//   const session = await getSession(context);
-
-//   if(!session){
-//     return{
-//       redirect:{
-//         destination:'/auth',
-//         permanent : false,
-//       }
-//     }
-//   }
-
-//   return{
-//     props: {}
-//   }
-  
-// }
-
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-   <h1 className="text-3xl bg-red-900 font-bold underline">
-      Hello world!
-    </h1>
-      
+      <h1 className="text-3xl bg-red-900 font-bold underline">Hello world!</h1>
+      <p className="text-white">{user?.email}</p>
+      <button onClick={() => signOut() } className="w-full bg-red-300 text-white">qayit</button>
     </>
-  )
+  );
 }
