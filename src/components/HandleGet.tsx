896e44f3427a5ac35ec5516ced26b7904028d79b
Aisha Mirza <84517249+ayshenm@users.noreply.github.com>
2024-02-25 08:06:58 +0400

    changed

--- src/components/HandleGet.tsx
+++ src/components/HandleGet.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Logo from "@/public/images/logo.png";
 import { getSession, signIn } from "next-auth/react";
@@ -8,23 +8,53 @@ import Input from "./Input";
 function HandleGet() {
   const [email, setEmail] = useState("");
   const router = useRouter();
-  
-  const [loading, setLoading] = useState(false);
 
-
-
-  
   const handleGetStartedClick = async () => {
     const session = await getSession();
 
     if (session) {
-      router.push("/auth");
+      router.push("/movie");
     } else {
       router.push("/profiles");
     }
   };
   return (
- 
+    // <div className=" justify-center items-center flex flex-col gap-5">
+    //   <p className="text-white text-lg">
+    //       Ready to watch? Enter your email to create or restart your membership.
+    //     </p>
+    //     <div className="flex justify-center items-center">
+    //     <div className="flex mt-4">
+    //       <input
+    //         placeholder="Email address"
+    //         className="bg-white p-4 min-w-[400px]"
+    //       />
+    //       </div>
+    //       <button
+    //         onClick={() => signIn()}
+    //         className="flex items-center bg-[#e50914] text-white text-xl px-8 py-"
+    //       >
+    //         Get Started
+    //         <svg
+    //           xmlns="http://www.w3.org/2000/svg"
+    //           fill="none"
+    //           viewBox="0 0 24 24"
+    //           stroke="currentColor"
+    //           className="w-6"
+    //         >
+    //           <path
+    //             strokeLinecap="round"
+    //             strokeLinejoin="round"
+    //             strokeWidth={2}
+    //             d="M9 5l7 7-7 7"
+    //           />
+    //         </svg>
+    //       </button>
+
+    //     </div>
+
+    // </div>
+
     <div className="flex flex-col ">
       <p className="text-white text-lg">
         Ready to watch? Enter your email to create or restart your membership.
@@ -36,7 +66,7 @@ function HandleGet() {
         </div>
         <div className="">
           <button
-            onClick={handleGetStartedClick}
+            onClick={() => signIn()}
             className="flex items-center bg-[#e50914] text-white text-xl px-8 py-3">
             Get Started
             <svg
